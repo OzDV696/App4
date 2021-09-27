3264b541266ee0f7ebc1f400ea5e19c5f6a93797
OzDV696 <OzDV696@users.noreply.github.com>
2021-09-26 23:46:53 -0500

    Agregar archivos de proyecto.

---
+++ App4/App4/ViewModels/CountViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App4.ViewModels
+{
+    public class CountViewModel : BaseViewModel
+    {
+        int _contador;
+        ICommand _buttonClickCommand;
+        ICommand _resetClickCommand;
+        string _countConverted;
+
+        public CountViewModel()
+        {
+            _contador = 0;
+        }
+
+        public int Contador {
+            get => _contador;
+            set
+            {
+                if (value == _contador) return;
+                _contador = value;
+                CountConverted = $"Haz dado click {_contador} veces";
+                OnPropertyChanged(nameof(CountConverted));
+            }
+        }
+
+        public string CountConverted
+        {
+            get => _countConverted;
+            set
+            {
+                if (string.Equals(_countConverted, value)) return;
+                _countConverted = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ButtonClickCommand
+        {
+            get => _buttonClickCommand ?? (_buttonClickCommand = new Command(ButtonClickAction));
+        }
+
+        public ICommand ResetClickCommand
+        {
+            get => _resetClickCommand ?? (_resetClickCommand = new Command(ResetClickAction));
+        }
+
+        private void ResetClickAction()
+        {
+            Contador = 0;
+            CountConverted = $"Haz reseteado el contador";
+        }
+
+        private void ButtonClickAction()
+        {
+            Contador++;
+        }
+    }
+}
